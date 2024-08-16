baa5714280a651511a9619d771f832dd53c7dbf8
Umar Shaikh <63943753+umarkali@users.noreply.github.com>
2024-08-17 00:23:42 +0530

    question-eight

---
+++ question-eight/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [text ,setText] = useState('')
+  const [todos,setTodos] = useState([])
+  const handleAdd=()=>{
+    if(text.trim()!== ''){
+      setTodos([...todos,text])
+      setText('')
+    }
+  }
+  const handleRemove = (index)=>{
+    const updateTodos = todos.filter((_,i)=> i!== index);
+    setTodos(updateTodos)
+
+  }
+  return (
+    <>
+      <div>
+        <input type='text' onChange={(e)=>setText(e.target.value)} />
+        <button onClick={handleAdd}>Add</button>
+        <ul>
+          <li>
+            {todos.map((todo,index)=><p key={index}>{todo} <button onClick={()=>handleRemove(index)}>delete</button> </p>)}
+            </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
